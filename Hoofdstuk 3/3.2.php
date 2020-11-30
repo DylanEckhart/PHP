9d2f9cbf9f8d753d05c0a7d2c54d002c12529974
Dylan Eckhart <deckhart@roc-dev.com>
2020-11-30 13:08:32 +0100

    Version 3.2

---
+++ Hoofdstuk 3/3.2.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<head>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+for ($a = 0; $a < 10; $a++) {
+    for ($b = 0; $b < $a; $b++) {
+        echo "*";
+    }
+    echo "*<br>";
+}
+
+?>
+</body>
+
+
