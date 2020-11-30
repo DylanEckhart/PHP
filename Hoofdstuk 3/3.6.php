0bad1c057f61873e448feb9375b0596144310b63
Dylan Eckhart <deckhart@roc-dev.com>
2020-11-30 13:47:41 +0100

    Version 3.6

---
+++ Hoofdstuk 3/3.6.php
@@ -0,0 +1,74 @@
+<?php
+
+$a = 25;
+$b = 32;
+$c = 11;
+$d = 23;
+
+?>
+
+<table border="1">
+    <tr>
+        <th>
+            De spartelkuikens
+        </th>
+        <th>
+            <?php
+            echo $a;
+            ?>
+        </th>
+        <th>
+            <?php
+            for ($i = 1; $i <= $a/5; $i++) {
+                echo "<img src='IMG/swim.png' width='50px' height='50px'>";
+            }
+            ?>
+        </th>
+    </tr>
+    <tr>
+        <th>
+            De Waterbuffels
+        </th>
+        <th>
+            <?php
+            for($i = 1; $i <= $b/5; $i++){
+                echo "<img src='IMG/swim.png' width='50px' height='50px'>";
+            }
+            ?>
+        </th>
+    </tr>
+    <tr>
+        <th>
+            Plonsmderin
+        </th>
+        <th>
+            <?php
+            echo $c;
+            ?>
+        </th>
+        <th>
+            <?php
+            for($i = 1; $i <= $b/5; $i++){
+            echo "<img src='IMG/swim.png' width='50px' height='50px'>";
+            }
+            ?>
+        </th>
+    </tr>
+    <tr>
+        <th>
+            Bommetje
+        </th>
+        <th>
+            <?php
+            echo $d;
+            ?>
+        </th>
+        <th>
+            <?php
+            for($i = 1; $i <= $b/5; $i++){
+            echo "<img src='IMG/swim.png' width='50px' height='50px'>";
+            }
+            ?>
+        </th>
+    </tr>
+</table>
