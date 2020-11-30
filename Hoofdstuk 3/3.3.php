6c69774c3c89e685b3761b85427a689da17ee4bd
Dylan Eckhart <deckhart@roc-dev.com>
2020-11-30 13:11:04 +0100

    Version 3.3

---
+++ Hoofdstuk 3/3.3.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<head>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+for($a = 35; $a >= 25; $a--){
+    echo "poppelepee<br>";
+}
+
+?>
+</body>
