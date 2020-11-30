435d8eba6e0090687f8d868919e33f0d07a15209
Dylan Eckhart <deckhart@roc-dev.com>
2020-11-30 14:15:56 +0100

    Version 3.6

--- Hoofdstuk 3/3.1.php
+++ Hoofdstuk 3/3.1.php
@@ -1,5 +1,5 @@
 <?php
 
-for($i = 0; $i < 9; $i++) {
+for($i = 0; $i < 10; $i++) {
     echo "<img src='IMG/f" . $i . ".jpg'>";
 }
---
+++ Hoofdstuk 3/3.7.php
@@ -0,0 +1,10 @@
+<?php
+
+//print ("De volgende momenten zijn nog beschikbaar:<ul>");
+//foreach($kappersagenda as ..... => .....) {
+//    if($afspraak == "") {
+//        print("<li>".$tijd."</li>") ;
+//    }
+//}
+
+
