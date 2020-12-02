f10e3162978904d92f7bf9f406c6796cb294352a
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-02 16:45:02 +0100

    Version 5.10

--- Hoofdstuk 5/Opdracht 4/index.php
+++ Hoofdstuk 5/Opdracht 4/index.php
@@ -14,30 +14,29 @@ function login($email, $password){
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>opdracht 5_3</title>
+    <title>Formulier PHP</title>
 </head>
 <body>
 <?php
-if(!isset($_POST['submit'])){?>
+    if(!isset($_POST['submit'])){?>
     <form method="post">
         Email:<input type="email" name="email"><br>
         Wachtwoord:<input type="password" name="password"><br>
         <input type="submit" name="submit">
     </form>
 <?php
-} else {
-    if(login($_POST['email'], $_POST['password'])){
-        echo "Welkom!";
-    }else{
-        echo "Sorry, geen toegang!";
+    } else {
+        if(login($_POST['email'], $_POST['password'])){
+            echo "Welkom!";
+        }else{
+            echo "Sorry, geen toegang!";
+        }
     }
-}
 ?>
-
 </body>
 </html>
