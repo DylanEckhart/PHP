d624e070b9432692a3d74732703f72d867bb61e4
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-02 09:28:47 +0100

    Version 5.2

--- Hoofdstuk 5/Opdracht 2/resultaat.php
+++ Hoofdstuk 5/Opdracht 2/resultaat.php
@@ -1,1 +1,24 @@
 <?php
+
+
+// Uitkomst bij geen invoer
+if ($_POST['naam'] == "") {
+    echo "Je moet nog een naam invullen.";
+    echo ">Terug naar het formulier.</a>";
+};
+
+if ($_POST['adres'] == "") {
+    echo "Je moet nog een adres invullen.";
+    echo ">Terug naar het formulier.</a>";
+};
+
+if ($_POST['email'] == "") {
+    echo "Je moet nog een email invullen.";
+    echo ">Terug naar het formulier.</a>";
+};
+
+if ($_POST['wachtwoord'] == "") {
+    echo "Je moet nog een wachtwoord invullen.";
+    echo ">Terug naar het formulier.</a>";
+};
+
