37330df8aba75a91db1b6e9253a9837d21606630
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-07 09:25:31 +0100

    Version 6.1

--- Hoofdstuk 6/index.php
+++ Hoofdstuk 6/index.php
@@ -1,1 +1,17 @@
 <?php
+
+// Variabelen
+$user = "root";
+$pass = "root";
+
+// Ophalen uit database PHP
+try {
+    $dbh = new PDO ('mysql:host=localhost;dbname=PHP;port=8889', $user, $pass);
+    foreach ($dbh -> query('SELECT * FROM cursist') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print ("Error!: ".$e -> getMessage(). "<br>");
+    die();
+}
