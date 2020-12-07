738b60e133019b42a1a0756110766232989a136c
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-07 08:56:29 +0100

    Version 5.12

--- Hoofdstuk 5/Opdracht 4/index.php
+++ Hoofdstuk 5/Opdracht 4/index.php
@@ -1,14 +1,14 @@
 <?php
 
-function login($email, $password){
+function login ($email, $password){
     $accounts = array();
     array_push($accounts, array("piet@worldonline.nl", "doetje123"));
     array_push($accounts, array("klaas@carpets.nl", "snoepje777"));
     array_push($accounts, array("truushendriks@wegweg.nl", "arkiearkie201"));
 
-    if(in_array(array($email, $password), $accounts)){
+    if (in_array(array($email, $password), $accounts)){
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -23,7 +23,7 @@ function login($email, $password){
 </head>
 <body>
 <?php
-    if(!isset($_POST['submit'])){?>
+    if (!isset($_POST['submit'])){?>
     <form method="post">
         Email:<input type="email" name="email"><br>
         Wachtwoord:<input type="password" name="password"><br>
@@ -31,9 +31,9 @@ function login($email, $password){
     </form>
 <?php
     } else {
-        if(login($_POST['email'], $_POST['password'])){
+        if (login($_POST['email'], $_POST['password'])){
             echo "Welkom!";
-        }else{
+        } else {
             echo "Sorry, geen toegang!";
         }
     }
