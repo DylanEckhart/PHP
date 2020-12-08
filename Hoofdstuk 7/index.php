ddd1bad7cbc7279808a3b5c240fd3ac41aa31d89
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-08 10:05:18 +0100

    Version 7.4

--- Hoofdstuk 7/index.php
+++ Hoofdstuk 7/index.php
@@ -18,20 +18,20 @@
 ?>
 
 <html>
-<body>
-<h1><?php echo $message; ?></h1>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <div class="form-group">
-        <label for="login">Login: </label>
-        <input type="text" name="login" value="">
-    </div>
-    <div class="form-group">
-        <label for="pwd">Password: </label>
-        <input type="password" name="pwd" value="">
-    </div>
-    <br>
-    <input type="submit" name="knop">
-</form>
-<p><a href="website.php">Website</a></p>
-</body>
+    <body>
+        <h1><?php echo $message; ?></h1>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <div class="form-group">
+                <label for="login">Login: </label>
+                <input type="text" name="login" value="">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password: </label>
+                <input type="password" name="pwd" value="">
+            </div>
+            <br>
+            <input type="submit" name="knop">
+        </form>
+        <p><a href="website.php">Website</a></p>
+    </body>
 </html>
