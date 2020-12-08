21f8aeeb288b4fab02b4c7c7fdda061412f02b5e
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-08 11:22:33 +0100

    Version 7.13

--- Hoofdstuk 7/admin.php
+++ Hoofdstuk 7/admin.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if (isset($_SESSION["gebruiker"] && $_SESSION["gebruiker"]["rol"] == "Admin")){
+    if (isset($_SESSION["gebruiker"]) && $_SESSION["gebruiker"]["rol"] == "Admin") {
         echo "<h1>Welkom ".$_SESSION["gebruiker"]. " op het admingedeelte van de website";
         echo "<p><a href='index.php'>Loginscherm</a></p>";
     } else {
