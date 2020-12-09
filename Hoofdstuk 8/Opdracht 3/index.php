7885f13a433201233f442cec08eb06d94a347f2d
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-09 15:49:18 +0100

    Version 8.10

--- Hoofdstuk 8/Opdracht 3/index.php
+++ Hoofdstuk 8/Opdracht 3/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Monkey Business</title>
+        <title>Autodealer</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
