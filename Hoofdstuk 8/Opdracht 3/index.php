ba608fdbb67960cabfb7592031834054288da6ca
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-05 10:05:20 +0100

    Version 8.13

--- Hoofdstuk 8/Opdracht 3/index.php
+++ Hoofdstuk 8/Opdracht 3/index.php
@@ -3,13 +3,23 @@
         <title>MR Wheely</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
     <div id="wrapper">
         <img src="IMG/wheelyHeader.jpg" alt="header-image">
         <form action="Autodealer.php" method="post">
+            <label>
+                Merk: <br>
+                <select id="merk">
+                    <option value='Audi'>Audi</option>
+                    <option value='Ferrari'>Ferrari</option>
+                    <option value='Fiat'>Fiat</option>
+                    <option value='Mercedes'>Mercedes</option>
+                    <option value='Opel'>Opel</option>
+                    <option value='Volkswagen'>Volkswagen</option>
+                </select>
+            </label>
 
         </form>
     </div>
