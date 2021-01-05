2c37c0b7c3ed97b78d238d8c892b1fb745cc46a2
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-05 11:53:53 +0100

    Version 8.17

--- Hoofdstuk 8/Opdracht 3/index.php
+++ Hoofdstuk 8/Opdracht 3/index.php
@@ -8,7 +8,7 @@
     <body>
         <div id="wrapper">
             <img class="header" src="IMG/wheelyHeader.jpg" alt="header-image">
-            <form action="Autodealer.php" method="post">
+            <form action="Autofilter.php" method="post">
                 <label>
                     Merk: <br>
                     <select id="merk">
@@ -32,23 +32,23 @@
                 <button type="reset">Reset</button>
             </form>
             <div id="autoModellen">
-                <div class='wheely-img' style='background-image:url("img/audi1.jpg");'><h5>Merk: Audi<br>Prijs : &euro; 102500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/audi2.jpeg");'><h5>Merk: Audi<br>Prijs : &euro; 108250.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/ferrari1.jpg");'><h5>Merk: Ferrari<br>Prijs : &euro; 99500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/ferrari2.jpg");'><h5>Merk: Ferrari<br>Prijs : &euro; 122500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/ferrari3.jpg");'><h5>Merk: Ferrari<br>Prijs : &euro; 152500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/fiat1.jpg");'><h5>Merk: Fiat<br>Prijs : &euro; 10500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/fiat2.jpg");'><h5>Merk: Fiat<br>Prijs : &euro; 11500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes1.jpg");'><h5>Merk: Mercedes<br>Prijs : &euro; 82500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes2.jpg");'><h5>Merk: Mercedes<br>Prijs : &euro; 132700.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes3.jpg");'><h5>Merk: Mercedes<br>Prijs : &euro; 87500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes4.jpg");'><h5>Merk: Mercedes<br>Prijs : &euro; 222650.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/opel1.jpg");'><h5>Merk: Opel<br>Prijs : &euro; 13500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/opel2.jpg");'><h5>Merk: Opel<br>Prijs : &euro; 9500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/opel3.jpg");'><h5>Merk: Opel<br>Prijs : &euro; 19500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/vw1.jpg");'><h5>Merk: Volkswagen<br>Prijs : &euro; 16340.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/vw2.jpg");'><h5>Merk: Volkswagen<br>Prijs : &euro; 18340.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/vw3.jpg");'><h5>Merk: Volkswagen<br>Prijs : &euro; 21670.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/audi1.jpg");'><h5>Merk: Audi<br>Prijs : €102500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/audi2.jpeg");'><h5>Merk: Audi<br>Prijs : €108250.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/ferrari1.jpg");'><h5>Merk: Ferrari<br>Prijs : €99500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/ferrari2.jpg");'><h5>Merk: Ferrari<br>Prijs : €122500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/ferrari3.jpg");'><h5>Merk: Ferrari<br>Prijs : €152500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/fiat1.jpg");'><h5>Merk: Fiat<br>Prijs : €10500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/fiat2.jpg");'><h5>Merk: Fiat<br>Prijs : €11500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/mercedes1.jpg");'><h5>Merk: Mercedes<br>Prijs : €82500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/mercedes2.jpg");'><h5>Merk: Mercedes<br>Prijs : €132700.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/mercedes3.jpg");'><h5>Merk: Mercedes<br>Prijs : €87500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/mercedes4.jpg");'><h5>Merk: Mercedes<br>Prijs : €222650.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/opel1.jpg");'><h5>Merk: Opel<br>Prijs : €13500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/opel2.jpg");'><h5>Merk: Opel<br>Prijs : €9500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/opel3.jpg");'><h5>Merk: Opel<br>Prijs : €19500.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/vw1.jpg");'><h5>Merk: Volkswagen<br>Prijs : €16340.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/vw2.jpg");'><h5>Merk: Volkswagen<br>Prijs : €18340.00</h5></div>
+                <div class='wheely-img' style='background-image:url("img/vw3.jpg");'><h5>Merk: Volkswagen<br>Prijs : €21670.00</h5></div>
             </div>
         </div>
     </body>
