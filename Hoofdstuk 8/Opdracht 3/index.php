4b91442c54697ea42f4979d480b87574e810a2dc
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-05 09:45:22 +0100

    Version 8.12

--- Hoofdstuk 8/Opdracht 3/index.php
+++ Hoofdstuk 8/Opdracht 3/index.php
@@ -1,37 +1,17 @@
 <html>
     <head>
-        <title>Autodealer</title>
+        <title>MR Wheely</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-    <div class="row">
-        <div class="col-xs-1"></div>
-        <div class="col-xs-10">
-            <form action="/wheely/index.php" method="post">
-                <div class="form-group">
-                    <label for="sel1">Merk:</label>
-                    <select class="form-control" id="merk" name="merk">
-                        <option value="">--Alle merken--</option>
-                        <option value='Audi' >Audi</option>
-                        <option value='Ferrari' >Ferrari</option>
-                        <option value='Fiat' >Fiat</option>
-                        <option value='Mercedes' >Mercedes</option>
-                        <option value='Opel' >Opel</option>
-                        <option value='Volkswagen' >Volkswagen</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="type">Minimale prijs:</label>
-                    <input type="text" class="form-control" id="type" name="minprijs" value="">
-                </div>
-                <div class="form-group">
-                    <label for="prijs">Maximale prijs:</label>
-                    <input type="text" class="form-control" id="prijs" name="maxprijs" value="">
-                </div>
-                <button type="submit" name="knop" value="submit" class="btn btn-default">Submit</button>
-            </form>
-    </body>
+    <div id="wrapper">
+        <img src="IMG/wheelyHeader.jpg" alt="header-image">
+        <form action="Autodealer.php" method="post">
 
+        </form>
+    </div>
+
+</html>
