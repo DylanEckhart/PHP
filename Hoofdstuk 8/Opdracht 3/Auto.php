c855e00da48050d7f2ea638eea18111f389d6cb3
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-06 12:03:03 +0100

    Opdracht opnieuw begonnen

---
+++ Hoofdstuk 8/Opdracht 3/Auto.php
@@ -0,0 +1,18 @@
+<?php
+
+class AutoOverzicht {
+    public array $autoos;
+
+    public function __construct() {
+        $this->autoos = [];
+    }
+
+    function getGefilterdeLijst($minimalePrijs, $maximalePrijs, $merk) {
+        $gefilterdeLijst = [];
+
+        foreach ($this->autoos as $auto) {
+
+
+        }
+    }
+}
--- Hoofdstuk 8/Opdracht 3/index.php
+++ Hoofdstuk 8/Opdracht 3/index.php
@@ -1,55 +1,10 @@
-<html>
-    <head>
-        <title>MR Wheely</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <div id="wrapper">
-            <img class="header" src="IMG/wheelyHeader.jpg" alt="header-image">
-            <form action="Autofilter.php" method="post">
-                <label>
-                    Merk: <br>
-                    <select id="merk">
-                        <option value='Audi'>Audi</option>
-                        <option value='Ferrari'>Ferrari</option>
-                        <option value='Fiat'>Fiat</option>
-                        <option value='Mercedes'>Mercedes</option>
-                        <option value='Opel'>Opel</option>
-                        <option value='Volkswagen'>Volkswagen</option>
-                    </select>
-                </label>
-                <label>
-                    Minimale prijs: <br>
-                    <input type="number" id="min-prijs">
-                </label>
-                <label>
-                    Maximale prijs: <br>
-                    <input type="number" id="max-prijs"><br>
-                </label>
-                <button type="submit">Submit</button>
-                <button type="reset">Reset</button>
-            </form>
-            <div id="autoModellen">
-                <div class='wheely-img' style='background-image:url("img/audi1.jpg");'><h5>Merk: Audi<br>Prijs: €102500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/audi2.jpeg");'><h5>Merk: Audi<br>Prijs: €108250.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/ferrari1.jpg");'><h5>Merk: Ferrari<br>Prijs: €99500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/ferrari2.jpg");'><h5>Merk: Ferrari<br>Prijs: €122500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/ferrari3.jpg");'><h5>Merk: Ferrari<br>Prijs: €152500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/fiat1.jpg");'><h5>Merk: Fiat<br>Prijs: €10500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/fiat2.jpg");'><h5>Merk: Fiat<br>Prijs: €11500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes1.jpg");'><h5>Merk: Mercedes<br>Prijs: €82500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes2.jpg");'><h5>Merk: Mercedes<br>Prijs: €132700.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes3.jpg");'><h5>Merk: Mercedes<br>Prijs: €87500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/mercedes4.jpg");'><h5>Merk: Mercedes<br>Prijs: €222650.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/opel1.jpg");'><h5>Merk: Opel<br>Prijs: €13500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/opel2.jpg");'><h5>Merk: Opel<br>Prijs: €9500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/opel3.jpg");'><h5>Merk: Opel<br>Prijs: €19500.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/vw1.jpg");'><h5>Merk: Volkswagen<br>Prijs: €16340.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/vw2.jpg");'><h5>Merk: Volkswagen<br>Prijs: €18340.00</h5></div>
-                <div class='wheely-img' style='background-image:url("img/vw3.jpg");'><h5>Merk: Volkswagen<br>Prijs: €21670.00</h5></div>
-            </div>
-        </div>
-    </body>
-</html>
+<?php
+
+include_once("Auto.php");
+
+$merk = isset($_GET["merk"]) ? $_GET["merk"] : "";
+$minimalePrijs = isset($_GET["minimalePrijs"]) ? $_GET["minimalePrijs"] : 0;
+$maximalePrijs =  isset($_GET["maximalePrijs"]) ? $_GET["maximalePrijs"] : 99999999;
+
+$autoOverzicht = new AutoOverzicht();
+
