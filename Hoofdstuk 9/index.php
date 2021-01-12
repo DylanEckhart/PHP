a3b4253237eaa48839067206ca5bab1782ee3bd5
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-12 09:27:21 +0100

    Version 9.1

---
+++ Hoofdstuk 9/index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <title>Broodjes</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>Bakkerij Vlecht Beheer</h1>
+</header>
+<div id="wrapper">
+    <ul>
+        <li>
+            <a href="index.php">Overzicht van de broodjes</a>
+        </li>
+        <li>
+            <a href="BroodjesToevoegen.php">Toevoegen van broodjes</a>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
