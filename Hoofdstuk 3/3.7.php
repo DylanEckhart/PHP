d0c482307992f2fdb2ad807d4b6389d1b372346f
Dylan Eckhart <deckhart@roc-dev.com>
2021-04-21 15:14:29 +0200

    Version 10.1

--- Hoofdstuk 3/3.7.php
+++ Hoofdstuk 3/3.7.php
@@ -1,10 +1,21 @@
+<html LANG="en">
+<head>
+    <title>Opdtracht 3.7</title>
+</head>
+<body>
 <?php
-
-//print ("De volgende momenten zijn nog beschikbaar:");
-//foreach($kappersagenda as ..... => .....) {
-//    if($afspraak == "") {
-//        print("<li>".$tijd."</li>") ;
-//    }
-//}
+$afspraak = "";
+$tijd = "";
+$kappersagenda = array($tijd = "9.15"=> $afspraak = "Mevr. Pietersen", $tijd = "9.30"=> $afspraak = "Mevr. Willems", $tijd = "9.45"=> $afspraak = "", $tijd = "10.00"=> $afspraak = "Paul van den Broek", $tijd = "10.15"=> $afspraak = "Karel de Meeuw", $tijd = "10.30"=> $afspraak = "");
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach ($kappersagenda as $tijd => $afspraak) {
+    if($afspraak == "") {
+        print("<li>".$tijd."</li>");
+    }
+}
+print("</ul>");
+?>
+</body>
+</html>
 
 
--- Hoofdstuk 7/admin.php
+++ Hoofdstuk 7/admin.php
@@ -2,7 +2,7 @@
     session_start();
 
     if (isset($_SESSION["gebruiker"]) && $_SESSION["gebruiker"]["rol"] == "Admin") {
-        echo "<h1>Welkom ".$_SESSION["gebruiker"]. " op het admingedeelte van de website";
+        echo "<h1>Welkom ".$_SESSION["gebruiker"]. " op het admingedeelte van de website</h1>";
         echo "<p><a href='index.php'>Loginscherm</a></p>";
     } else {
         header('location: index.php');
